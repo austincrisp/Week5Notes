f2175eb5b75b2befcd847208ff550be7fa9f8035
austincrisp <austi@AUSTINBLADE>
2017-02-09 12:20:37 -0500

    MVC with user authentication

---
+++ Day4MVCUser/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Day4MVCUser.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Pages { get; set; }
+        public string Genre { get; set; }
+
+        public string OwnerId { get; set; }
+
+        [ForeignKey("OwnerId")]
+        public virtual ApplicationUser Owner { get; set; }
+    }
+}
